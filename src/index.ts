15d057991ff97d4c4affaab0fc75c9858b3d8d0e
Gabriela <104107313+mgbgabriela@users.noreply.github.com>
2022-08-13 23:51:50 -0300

    Initial commit

---
+++ src/index.ts
@@ -0,0 +1,41 @@
+let input1 = document.getElementById("unitario");
+let input2 = document.getElementById("nombre");
+let btnIngresar = document.getElementById("btnIngresar");
+let btnTotal = document.getElementById("btnTotal");
+let btnDescuento = document.getElementById("btnDescuento");
+let lista = document.getElementById("lista");
+let precioTotal: number = 0;
+let arrProducto: number[] = [];
+let arrNombre: string[] = [];
+
+function agregarProductos(): number[] {
+  let producto = Number(input1.value);
+  let nombreProducto = input2?.value;
+  arrNombre.push(nombreProducto);
+  arrProducto.push(producto);
+
+  lista?.innerHTML += `<li>${nombreProducto} :$ ${producto}</li> `;
+}
+
+btnIngresar?.addEventListener("click", agregarProductos);
+
+function sumarTotal(): number {
+  for (let i: number = 0; i < arrProducto.length; i++) {
+    precioTotal += arrProducto[i];
+  }
+
+  lista?.innerHTML += `El total de la compra es $ ${precioTotal.toString()} `;
+}
+
+btnTotal?.addEventListener("click", sumarTotal);
+
+function calcularDescuento(): number {
+  if (precioTotal > 2500) {
+    let totalConDescuento: number = precioTotal - precioTotal * 0.1;
+    descuento.innerHTML += `Tiene un descuento del 10%  su total es $ ${totalConDescuento}`;
+  } else {
+    descuento.innerHTML += `Con su compra de $2500 puede acceder a un descuento`;
+  }
+}
+
+btnDescuento?.addEventListener("click", calcularDescuento);
